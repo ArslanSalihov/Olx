5d59eb1907cbcb29f0dadd9c7f536bac5392413a
Arslan Salihov <salihovarslan555@gmail.com>
2022-08-10 12:25:02 +0500

    third comment

--- olx_app/views.py
+++ olx_app/views.py
@@ -15,11 +15,13 @@ from olx_app.forms import RegisterForm
 
 def index(request):
     product = Product.objects.all()
-    return render(request,'olx_app/index.html')
+    category = Category.objects.all()
+    return render(request,'olx_app/index.html', {'product':product, 'category':category})
 
 def c_product(request):
     product = Product.objects.all()
-    return render(request,'olx_app/c_product.html')
+    category = Category.objects.all()
+    return render(request,'olx_app/c_product.html', {'product':product, 'category':category})
 
 def create(request):
     if request.method == 'POST':
@@ -28,16 +30,19 @@ def create(request):
         product.text = request.POST.get('text')
         product.price = request.POST.get('price')
         product.author = User.objects.get(id=request.user.id)
+        category = Category.objects.all()
         if request.FILES.get('image',False) !=False:
             myfile = request.FILES['image']
             fs = FileSystemStorage()
             filename = fs.save(myfile.name,myfile)
             product.image = myfile
         product.save()
-    return redirect('index')
+        return redirect('index')
+    return render(request, 'olx_app', {'product':product, 'category':category})
+
 
 def update(request,id):
-    product = Product.objects.get (id = id) 
+    product = Product.objects.get(id = id)
     if request.method == 'POST':
         product.title = request.POST.get('title')
         product.text = request.POST.get('text')
@@ -57,7 +62,7 @@ def delete_post(request,id):
     if request.method == 'POST':
         product.delete()
         return redirect(reverse('index'))
-    return render(request, 'olx_app/delete.html',{'product':product})
+    return render(request, 'olx_app',{'product':product})
 
 def register(request):
     if request.method == 'POST':
@@ -79,19 +84,20 @@ def login(request):
         form = RegisterForm()
     return render(request,'olx_app/registration/login.html', {'form':form})   
 
-def product_detail(request):
-    product = Product.objects.all()
-    return render(request,'olx_app/product_detail.html')
-
-def category_detail(request):
+def product_detail(request,id):
+    product = Product.objects.get(id = id)
     category = Category.objects.all()
+    return render(request,'olx_app/product_detail.html', {'product':product , 'category':category})
+
+def category_detail(request,id):
+    category = Category.objects.get(id = id)
     product = Product.objects.all()
-    return render(request, 'olx_app/category_detail.html',{'category':category})
+    return render(request, 'olx_app/category_detail.html',{'category':category , 'product':product})
 
 def search(request):
     quary = request.GET.get('search')
-    search_obj= Product.objects.filter(Q(title__contains=quary))
-    return render(request, 'olx_app/search.html',{'quary':quary, 'search_obj': search_obj})
+    search_obj = Product.objects.filter(Q(title__icontains=quary))
+    return render(request, 'olx_app/search.html', {'quary':quary, 'search_obj': search_obj})
 
 def add_to_favourite(request,product_id):
     product = Product.objects.get(id=product_id)
